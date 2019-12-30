fe5f9b0f0b05c48ad6b7483389144e374635d3f2
Dmitry-IWS <dmitry1991.my@mail.ru>
2019-12-30 14:24:49 +0300

    hack 9

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -31,8 +31,8 @@ export const PostScreen = ({ navigation }) => {
   }, [booked])
 
   const toggleHandler = useCallback(() => {
-    dispatch(toogleBooked(postId))
-  }, [dispatch, postId])
+    dispatch(toogleBooked(post))
+  }, [dispatch, post])
 
   useEffect(() => {
     navigation.setParams({ toggleHandler })
--- src/store/actions/post.js
+++ src/store/actions/post.js
@@ -1,30 +1,55 @@
+import * as FileSystem from 'expo-file-system'
 import { LOAD_POSTS, TOGGLE_BOOKED, REMOVE_POST, ADD_POST } from '../types'
+import { DB } from '../../db'
 
 export const loadPosts = () => {
-  return {
-    type: LOAD_POSTS,
-    payload: []
+  return async dispatch => {
+    const posts = await DB.getPosts()
+
+    dispatch({
+      type: LOAD_POSTS,
+      payload: posts
+    })
   }
 }
 
-export const toogleBooked = id => {
-  return {
+export const toogleBooked = post => async dispatch => {
+  await DB.updatePost(post)
+
+  dispatch({
     type: TOGGLE_BOOKED,
-    payload: id
-  }
+    payload: post.id
+  })
 }
 
-export const removePost = id => {
-  return {
+export const removePost = id => async dispatch => {
+  await DB.removePost(id)
+  dispatch({
     type: REMOVE_POST,
     payload: id
-  }
+  })
 }
 
-export const addPost = post => {
-  post.id = Date.now().toString()
-  return {
-    type: ADD_POST,
-    payload: post
+export const addPost = post => async dispatch => {
+  const fileName = post.img.split('/').pop()
+  const newPath = FileSystem.documentDirectory + fileName
+
+  try {
+    await FileSystem.moveAsync({
+      to: newPath,
+      from: post.img
+    })
+  } catch (e) {
+    console.log('Error:', e)
   }
+
+  const payload = { ...post, img: newPath }
+  const id = await DB.createPost(payload)
+
+  payload.id = id
+
+  dispatch({
+    type: ADD_POST,
+    payload
+  })
 }
