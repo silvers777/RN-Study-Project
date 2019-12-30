5ff5dc923a87561ba9b70ce0de3b56881c54647d
dmitry-iws <dmitry1991.my@mail.ru>
2019-12-30 12:51:28 +0300

    hack 8

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,8 +1,16 @@
 import React from 'react'
-import { View, StyleSheet, FlatList } from 'react-native'
+import { View, StyleSheet, FlatList, Text } from 'react-native'
 import { Post } from './Post'
 
 export const PostList = ({ data, onOpen }) => {
+  if (!data.length) {
+    return (
+      <View>
+        <Text style={styles.noItems}>Постов пока нет!</Text>
+      </View>
+    )
+  }
+
   return (
     <View style={styles.wrapper}>
       <FlatList
@@ -17,5 +25,11 @@ export const PostList = ({ data, onOpen }) => {
 const styles = StyleSheet.create({
   wrapper: {
     padding: 10
+  },
+  noItems: {
+    fontFamily: 'open-regular',
+    textAlign: 'center',
+    marginVertical: 10,
+    fontSize: 18
   }
 })
